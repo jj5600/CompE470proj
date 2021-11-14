507f263a84986b7c794d6711ddcab9b25b7ddc53
jj5600 <57789684+jj5600@users.noreply.github.com>
2021-11-14 12:43:38 -0800

    Create ss1tb.v

---
+++ code/ss1tb.v
@@ -0,0 +1,15 @@
+module SHA256_tb();
+reg[31:0] W1_tb;
+wire[31:0] s1_tb;
+s_sigma_one dut(.W1(W1_tb),.s1(s1_tb));
+Initial
+begin
+#5
+W1_tb=32'b01101111001000000111011101101111;
+$stop;
+end
+initial 
+begin
+$monitor("%b",W1_tb);
+$monitor("%b",s1_tb);
+end
