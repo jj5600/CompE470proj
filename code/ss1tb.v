07bf8cf6c96908e85983cde0814bd673fa1b42c6
jj5600 <57789684+jj5600@users.noreply.github.com>
2021-12-03 20:29:28 -0800

    Update ss1tb.v

--- code/ss1tb.v
+++ code/ss1tb.v
@@ -2,7 +2,7 @@ module SHA256_tb();
 reg[31:0] W1_tb;
 wire[31:0] s1_tb;
 s_sigma_one dut(.W1(W1_tb),.s1(s1_tb));
-Initial
+initial
 begin
 #5
 W1_tb=32'b01101111001000000111011101101111;
@@ -13,3 +13,4 @@ begin
 $monitor("%b",W1_tb);
 $monitor("%b",s1_tb);
 end
+endmodule 
